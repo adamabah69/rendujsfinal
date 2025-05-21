181e6793dfee87eb2e6d6a5321934fe905bc0af7
adamabah69 <adama.bah@latoile.em-lyon.com>
2025-05-21 17:26:30 +0200

    aprem21mai

--- script.js
+++ script.js
@@ -1,23 +1,22 @@
-const container = document.getElementById;
+const container = document.getElementById(container);
+const button = document.querySelectorAll(button);
 
 
 fetch("https://js-dynamic-portfolio-data-makerslab-emlyon-cdweb-8f83155c64a0cc.gitlab.io/json/patisserie.json")
-.then(response => response.json())
-.then(data => {
-    console.log(data);
-    console.log(data.nomCommercial);
-    console.log(data.phraseAccroche);
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        console.log(data.nomCommercial);
+        console.log(data.phraseAccroche);
 
+function data(patisserie) {
+console.log(data);
 
-})
-
-
-function data(produits) {
-    console.log(data);
-    document.createElement(data.nomCommercial);
-    document.createElement(data.phraseAccroche);
-    document.createElement(data.button);
+    document.createElement();
 }
+    })
+
+
 
 
 
